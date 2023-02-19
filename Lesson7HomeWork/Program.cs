16691f6d1e868a9361f60f3f236d314ecdee0a7c
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2023-02-19 13:07:08 +0300

    Initial commit

---
+++ Lesson7HomeWork/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson7HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
