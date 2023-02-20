e55caaa6cf2a34bcb826009ffc6b8b4122b5540c
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2023-02-20 15:19:41 +0300

    Task 1.1

---
+++ Lesson7HomeWork/Phone.cs
@@ -0,0 +1,52 @@
+﻿
+
+namespace Lesson7HomeWork
+{
+    // For Task 1.1--------------------------------------------------------------
+    internal class Phone
+    {
+        public string number { get; set; }
+        public string model { get; set; }
+        public int weight { get; set; }
+
+        
+        public Phone( string number, string model, int weight) : this(number, model)
+        {
+            this.weight = weight;
+        }
+
+        public Phone(string number, string model) 
+        {
+            this.number = number;
+            this.model = model;
+        }
+
+        public Phone() { }
+        
+        
+        public void recieveCall(string name)
+        {
+            Console.WriteLine($"Звонит {name}");
+        }
+
+        public void recieveCall(string name, string number)
+        {
+            Console.WriteLine($"Звонит {name} \n.......{number}");
+        }
+
+        public string getNumber()
+        {
+
+            return number;
+        }
+
+        // проверить как работает
+        public void sendMessage(params string[] number)
+        {
+            foreach (var item in number)
+            {
+                Console.WriteLine($"Сообщение отправлено на номер {item}");
+            }
+        }
+    }
+}
--- Lesson7HomeWork/Program.cs
+++ Lesson7HomeWork/Program.cs
@@ -2,9 +2,55 @@
 {
     internal class Program
     {
+        // For Task 1.1--------------------------------------------------------------
+        public static void sendMessage(params string[] number)
+        {
+            foreach (var item in number)
+            {
+                Console.WriteLine($"Сообщение отправлено на номер {item}");
+            }
+        }
+        // -----------------------------------------------------------------------
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Task 1.1--------------------------------------------------------------
+            
+            Phone nokia = new Phone("079876987", "Нокиа", 383);
+            Console.WriteLine($"Номер: {nokia.number}, модель: {nokia.model}, вес: {nokia.weight}");
+            nokia.recieveCall("Alex");
+            Console.WriteLine(nokia.getNumber());
+
+            Phone sony = new Phone("235645635", "Сони");
+            Console.WriteLine($"Номер: {sony.number}, модель: {sony.model}, вес: {sony.weight}");
+            sony.recieveCall("Inga");
+            Console.WriteLine(sony.getNumber());
+            
+            Phone apple = new Phone();
+            Console.WriteLine($"Номер: {apple.number}, модель: {apple.model}, вес: {apple.weight}");
+            apple.recieveCall("Maxim");
+            Console.WriteLine(apple.getNumber());
+
+            Console.WriteLine("--------------перегрузка метода recieveCall() ----------------------");
+
+            nokia.recieveCall("Alex", nokia.getNumber());
+            sony.recieveCall("Inga", sony.number);
+            apple.recieveCall("Maxim", "98768765876");
+
+            string[] numbers = new[] {nokia.number, sony.number, apple.number};
+            sendMessage(numbers);
+
+
+            // Task 1.2 ----------------------------------------------------------------------
+
+
+
+            // Task 2.1 (основное задание)
+
+
+
+
         }
     }
 }
